ad3d66454dead525a8158b4efabfc29fd8aa0177
Training_C2a.05.01 <varsha.sridhar@accenture.com>
2017-11-24 16:35:31 +0530

    Trigger Autogit push origin master

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -30,7 +30,7 @@ public class HelloTest {
 	@Test
 	public void f() {
 
-		text = driver.findElement(By.xpath("html/body/h2"));
+		//text = driver.findElement(By.xpath("html/body/h2"));
 		Assert.assertEquals(driver.getTitle(), "");
 		System.out.println("Test Passed");
 
